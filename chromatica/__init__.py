5c65c5ee87f3672d7a137f043fffd35f2f8da67d
snail <snail1704@gmail.com>
2024-11-15 17:35:53 +0400

    Add base dataset, without preprocessing

---
+++ chromatica/__init__.py
@@ -0,0 +1,11 @@
+"""
+Chromatica is CNN for colorization photos.
+
+This package provides tools for:
+- Load and prepare dataset
+- Train and test CNN
+- Use CNN for colorize photos
+
+For more details, visit: https://github.com/snailUlitka/chromatica
+Aurhor: snail <snail1704@gmail.com>
+"""
