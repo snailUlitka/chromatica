258788dc28101e8c132e7e4fe54de7b8a90510b3
Mikhail Polevoda <snail1704@gmail.com>
2025-05-26 20:09:57 +0400

    [#21]: update draft for train, add for test

--- src/chromatica/datasets/dataset.py
+++ src/chromatica/datasets/dataset.py
@@ -15,8 +15,33 @@ The Dataset class offers:
 # TODO: Implement lazy and parallel load logic
 # https://github.com/snailUlitka/chromatica/issues/17
 
+from pathlib import Path
+
+import torch
 from torch.utils.data import Dataset
 
 
 class ImageDataset(Dataset):
     """Not implemented yet."""
+
+    def __init__(self, path_to_dir: Path) -> None:
+        super().__init__()
+        assert path_to_dir
+        raise NotImplementedError
+
+    def __len__(self) -> int:
+        """Not implemented yet."""
+        raise NotImplementedError
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        """Not implemented yet."""
+        assert idx
+        raise NotImplementedError
+
+    def __getitems__(
+        self,
+        idxs: list[int],
+    ) -> list[tuple[torch.Tensor, torch.Tensor]]:
+        """Not implemented yet."""
+        assert idxs
+        raise NotImplementedError
